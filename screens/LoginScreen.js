2832a0afc5da5efb6644c27cae2349b51882b9fa
Leon <leon.yee000@gmail.com>
2024-02-17 22:56:55 -0500

    uploaded assets

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,23 +1,24 @@
 import { StyleSheet, Text, View, Button } from 'react-native';
+import { useFonts } from 'expo-font';
 
 import GLOBAL from '../global.js';
 
-const LoginScreen = ({navigation}) => {
+const LoginScreen = ({ navigation }) => {
 
-    function login() {
-        GLOBAL.loggedIn = true;
-        navigation.navigate('Home', { screen: 'Home' });
-    }
+	function login() {
+		GLOBAL.loggedIn = true;
+		navigation.navigate('Home', { screen: 'Home' });
+	}
 
-    return (
-        <View>
-            <Text>This is the Login Screen</Text>
-            <Button
-                title="Go to Home"
-                onPress={() => login()}
-            />
-        </View>
-    );
+	return (
+		<View>
+			<Text>This is the Login Screen</Text>
+			<Button
+				title="Go to Home"
+				onPress={() => login()}
+			/>
+		</View>
+	);
 }
 
 export default LoginScreen;
