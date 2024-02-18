76f5d2b97d011f3c7e203ca94427b0dcaecea9ec
calebchua <chua17@purdue.edu>
2024-02-18 02:43:38 -0500

    login screen image and text input

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,32 +1,54 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, TextInput, Keyboard, TouchableWithoutFeedback } from 'react-native';
 
 import GLOBAL from '../global.js';
 
 const LoginScreen = ({ navigation }) => {
 
-	function login() {
-		GLOBAL.loggedIn = true;
-		navigation.navigate('Home', { screen: 'Home' });
-	}
+    function login() {
+        GLOBAL.loggedIn = true;
+        navigation.navigate('Home', { screen: 'Home' });
+    }
 
-	return (
-		<View className="flex-1 h-screen">
-			<View className="flex-1">
-				<Text>This is the Login Screen</Text>
-				<Button
-					title="Go to Home"
-					onPress={() => login()}
-				/>
-			</View>
-			<View className="my-12">
-				<Text className="text-center">Don't have an account?</Text>
-				<Button
-					title="Sign up!"
-					className="text-red-300"
-					onPress={() => navigation.navigate('Signup')} />
-			</View>
-		</View>
-	);
+    return (
+        <View className="flex-1 h-screen bg-white">
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+                <View className="flex-1 items-center">
+                    <Image className="object-scale-down h-48 w-48"
+                        source={require('../assets/logo.png/')}
+                    />
+                    <LoginTextInput
+                        placeholder="Username or Email"
+                        secureTextEntry={false}
+                    />
+                    <LoginTextInput
+                        placeholder="Password"
+                        secureTextEntry={true}
+                    />
+                    <Button
+                        title="Login"
+                        onPress={() => login()}
+                    />
+                </View>
+            </TouchableWithoutFeedback>
+            <View className="my-12">
+                <Text className="text-center">Don't have an account?</Text>
+                <Button
+                    title="Sign up!"
+                    className="text-red-300"
+                    onPress={() => navigation.navigate('Signup')} />
+            </View>
+        </View>
+    );
+}
+
+function LoginTextInput({ placeholder, secureTextEntry }) {
+    return (
+        <TextInput
+            placeholder={placeholder}
+            className="bg-gray-300 text-gray-500 w-3/4 rounded-xl py-3 px-2 my-2"
+            secureTextEntry={secureTextEntry}
+        />
+    )
 }
 
 export default LoginScreen;
