dbd46e99360b091f2371a04434cd8134417b7c07
charlie-schmdt <schmi302@purdue.edu>
2024-02-17 18:13:22 -0600

    Created UI for change password

--- screens/AccountContainer.js
+++ screens/AccountContainer.js
@@ -1,14 +1,54 @@
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text, TextInput, View, Button, Alert } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 const Stack = createNativeStackNavigator();
+const checkPasswords = (currentPass, newPass, reEnter) => {
+  // check if the currentPass = the user's password
+
+  // check if the new passwords match
+  if (newPass != reEnter) {
+    Alert.alert("Passwords don't match!");
+    return;
+  }
+  Alert.alert('Passwords Entered!');
+}
 
 const EditPasswordScreen = ({navigation}) => {
+  
+  const [currentPassword, onChangeCur] = React.useState('');
+  const [newPassword, onChangeNew] = React.useState('');
+  const [reEnterPassword, onChangeRe] = React.useState('');
+
+  // NOTE: disabled function for the button causes delay when entering text input
   return (
-    <View style={styles.container}>
-      <Text>Edit your password!</Text>
-    </View>
+    <SafeAreaView>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeCur}
+        value={currentPassword}
+        placeholder="Enter current password"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNew}
+        value={newPassword}
+        placeholder="Enter new password"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeRe}
+        value={reEnterPassword}
+        placeholder="Re-enter new password"
+      />
+      <Button
+        color='#ff5661'
+        title="Done"
+        disabled={!Boolean(currentPassword && newPassword && reEnterPassword)}
+        onPress={() => checkPasswords(currentPassword, newPassword, reEnterPassword)}
+      />
+    </SafeAreaView>
   )
 }
 
@@ -41,6 +81,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
 
