f19bf5123b6a8cf4a480cff895709adc70753c1f
calebchua <calebwchua@gmail.com>
2024-02-18 19:19:56 -0500

    fixed indentation

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -5,54 +5,54 @@ import Textbox from './components/Textbox.js'
 import GLOBAL from '../global.js';
 
 const LoginScreen = ({ navigation }) => {
-    const [password, setPassword] = useState('');
-    const [usernameEmail, setUsernameEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [usernameEmail, setUsernameEmail] = useState('');
 
-    function login() {
-        GLOBAL.loggedIn = true;
+	function login() {
+		GLOBAL.loggedIn = true;
 
-        console.log('Username/Email: ', usernameEmail);
-        console.log('Password: ', password);
+		console.log('Username/Email: ', usernameEmail);
+		console.log('Password: ', password);
 
-        navigation.navigate('Home', { screen: 'Home' });
-    }
+		navigation.navigate('Home', { screen: 'Home' });
+	}
 
-    return (
-        <View className="flex-1 h-screen bg-white">
-            <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-                <View className="flex-1 items-center mt-20">
-                    <Image className="object-scale-down h-48 w-48"
-                        source={require('../assets/logo.png/')}
-                    />
-                    <Textbox
-                        state={usernameEmail}
-                        setState={setUsernameEmail}
-                        placeholder="Username or Email"
-                        secureTextEntry={false}
-                    />
-                    <Textbox
-                        state={password}
-                        setState={setPassword}
-                        placeholder="Password"
-                        secureTextEntry={true}
-                        onChangeText={setPassword}
-                    />
-                    <Pressable
-                        className="bg-primary w-1/2 rounded-xl py-5 mt-3"
-                        onPress={() => login()}
-                    >
-                        <Text className="text-white font-bold text-center text-lg">LOGIN</Text>
-                    </Pressable>
-                </View>
-            </TouchableWithoutFeedback>
-            <View className="my-12 flex-row justify-center space-x-1">
-                <Text className="text-secondary">Don't have an account?</Text>
-                <Pressable onPress={() => navigation.navigate('Signup')}>
-                    <Text className="text-primary">Sign up!</Text>
-                </Pressable>
-            </View>
-        </View>
-    );
+	return (
+		<View className="flex-1 h-screen bg-white">
+			<TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+				<View className="flex-1 items-center mt-20">
+					<Image className="object-scale-down h-48 w-48"
+						source={require('../assets/logo.png/')}
+					/>
+					<Textbox
+						state={usernameEmail}
+						setState={setUsernameEmail}
+						placeholder="Username or Email"
+						secureTextEntry={false}
+					/>
+					<Textbox
+						state={password}
+						setState={setPassword}
+						placeholder="Password"
+						secureTextEntry={true}
+						onChangeText={setPassword}
+					/>
+					<Pressable
+						className="bg-primary w-1/2 rounded-xl py-5 mt-3"
+						onPress={() => login()}
+					>
+						<Text className="text-white font-bold text-center text-lg">LOGIN</Text>
+					</Pressable>
+				</View>
+			</TouchableWithoutFeedback>
+			<View className="my-12 flex-row justify-center space-x-1">
+				<Text className="text-secondary">Don't have an account?</Text>
+				<Pressable onPress={() => navigation.navigate('Signup')}>
+					<Text className="text-primary">Sign up!</Text>
+				</Pressable>
+			</View>
+		</View>
+	);
 }
 
 export default LoginScreen;
--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -7,73 +7,73 @@ import Textbox from './components/Textbox.js'
 import GLOBAL from '../global.js';
 
 const SignupScreen = ({ navigation }) => {
-	const [username, setUsername] = useState('');
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
-	const [retypedPassword, setRetypePassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [retypedPassword, setRetypePassword] = useState('');
 
 
-	function signup() {
-		GLOBAL.loggedIn = true;
+  function signup() {
+    GLOBAL.loggedIn = true;
 
-		console.log('Username:', username);
-		console.log('Email:', email);
-		console.log('Password:', password);
-		console.log('Retyped Password:', retypedPassword);
+    console.log('Username:', username);
+    console.log('Email:', email);
+    console.log('Password:', password);
+    console.log('Retyped Password:', retypedPassword);
 
-		navigation.navigate('Home', { screen: 'Home' });
-	}
+    navigation.navigate('Home', { screen: 'Home' });
+  }
 
-	return (
-		<View className="flex-1 h-screen bg-white">
-			<TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-				<View className="flex-1 items-center mt-20">
-					<View className='flex flex-row w-3/4'>
-						<Pressable onPress={() => navigation.navigate('Login')} className="pt-1.5 mr-2">
-							<AntDesign name="caretleft" size={24} style={{ color: '#fe575f' }} />
-						</Pressable>
-						<Text className="text-3xl font-bold mb-20 text-left text-primary">Create Account</Text>
-					</View>
-					<Textbox
-						placeholder="Username"
-						secureTextEntry={false}
-						state={username}
-						setState={setUsername}
-					/>
-					<Textbox
-						placeholder="Email address"
-						secureTextEntry={false}
-						state={email}
-						setState={setEmail}
-					/>
-					<Textbox
-						placeholder="Password"
-						secureTextEntry={true}
-						state={password}
-						setState={setPassword}
-					/>
-					<Textbox
-						placeholder="Re-enter password"
-						secureTextEntry={true}
-						state={retypedPassword}
-						setState={setRetypePassword}
-					/>
-					<Pressable
-						className="bg-primary w-1/2 rounded-xl py-5 mt-3"
-						onPress={() => signup()}
-					>
-						<Text className="text-white font-bold text-center text-lg">SIGNUP</Text>
-					</Pressable>
-				</View>
-			</TouchableWithoutFeedback>
-			<View className="my-12 flex-row justify-center space-x-1">
-				<Text className="text-secondary">Already have an account?</Text>
-				<Pressable onPress={() => navigation.navigate('Login')}>
-					<Text className="text-primary">Log in!</Text>
-				</Pressable>
-			</View>
-		</View>
-	);
+  return (
+    <View className="flex-1 h-screen bg-white">
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+        <View className="flex-1 items-center mt-20">
+          <View className='flex flex-row w-3/4'>
+            <Pressable onPress={() => navigation.navigate('Login')} className="pt-1.5 mr-2">
+              <AntDesign name="caretleft" size={24} style={{ color: '#fe575f' }} />
+            </Pressable>
+            <Text className="text-3xl font-bold mb-20 text-left text-primary">Create Account</Text>
+          </View>
+          <Textbox
+            placeholder="Username"
+            secureTextEntry={false}
+            state={username}
+            setState={setUsername}
+          />
+          <Textbox
+            placeholder="Email address"
+            secureTextEntry={false}
+            state={email}
+            setState={setEmail}
+          />
+          <Textbox
+            placeholder="Password"
+            secureTextEntry={true}
+            state={password}
+            setState={setPassword}
+          />
+          <Textbox
+            placeholder="Re-enter password"
+            secureTextEntry={true}
+            state={retypedPassword}
+            setState={setRetypePassword}
+          />
+          <Pressable
+            className="bg-primary w-1/2 rounded-xl py-5 mt-3"
+            onPress={() => signup()}
+          >
+            <Text className="text-white font-bold text-center text-lg">SIGNUP</Text>
+          </Pressable>
+        </View>
+      </TouchableWithoutFeedback>
+      <View className="my-12 flex-row justify-center space-x-1">
+        <Text className="text-secondary">Already have an account?</Text>
+        <Pressable onPress={() => navigation.navigate('Login')}>
+          <Text className="text-primary">Log in!</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
 }
 
 export default SignupScreen;
