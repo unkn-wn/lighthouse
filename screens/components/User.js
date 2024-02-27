28dc4fb59c2f32ec919978def2fa30046cde2b94
Caleb Chua <112199132+calebchua@users.noreply.github.com>
2024-02-27 17:05:57 -0500

    Additional user fields in User.js
    Added fields to User class

--- screens/components/User.js
+++ screens/components/User.js
@@ -1,6 +1,9 @@
 export default class User {
-    constructor(username, email) {
+    constructor(username, email, uid, permitType, vehicleType) {
         this.username = username;
         this.email = email;
+        this.uid = uid;
+        this.permitType = permitType;
+        this.vehicleType = vehicleType;
     }
 }
