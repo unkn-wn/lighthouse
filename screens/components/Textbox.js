22da0370907271c32554d7515a6293cb5a671f2a
Caleb Chua <112199132+calebchua@users.noreply.github.com>
2024-02-28 23:51:05 -0500

    Added functionality to edit username
    Implemented username changing in profile screen

--- screens/components/Textbox.js
+++ screens/components/Textbox.js
@@ -1,14 +1,23 @@
 import { TextInput } from 'react-native';
 
 const Textbox = (props) => {
+    var className;
+    if (props.editable == false) {
+        className = "bg-gray-300 text-black w-3/4 rounded-xl py-3 px-2 my-2";
+    }
+    else {
+        className = "bg-gray-300 text-gray-500 w-3/4 rounded-xl py-3 px-2 my-2";
+    }
+
     return (
         <TextInput
             placeholder={props.placeholder}
-            className="bg-gray-300 text-gray-500 w-3/4 rounded-xl py-3 px-2 my-2"
+            className={className}
             secureTextEntry={props.secureTextEntry}
             editable={props.editable}
             value={props.value}
             onChangeText={props.setState}
+            onSubmitEditing={props.onSubmitEditing}
             {...props.state}
         />
     )
