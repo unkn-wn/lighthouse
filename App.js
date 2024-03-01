757348dc5b6a6c66f5a0a7ab50332d5df8caf2e3
Phineas1500 <sriram.kiron@gmail.com>
2024-02-29 23:14:26 -0500

    disabled react native @leon

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
+LogBox.ignoreAllLogs();//Ignore all log notifications
+
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack';
